dc57426aef02a0903e8cc51a881b8e9d5c002ec3
Joshua Bassin <jbassin@bu.edu>
2019-03-05 21:27:45 -0500

    Namespaced to functions and option

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { curry2, curry3 } = require('./src/curry/curry');
 const { reduce, reduceR, reduceL } = require('./src/reduce/curriedReduce');
 
-module.exports = {
+const Option = require('./src/option/option');
+
+const Functions = {
     curry2, curry3,
     reduce, reduceR, reduceL,
+};
+
+module.exports = {
+    Functions,
+    Option,
 };
